dc460a20fbc8c340c8f71af0641efe229367ff0f
iskcal <iskcal@outlook.com>
2020-08-28 15:22:28 +0800

    添加项目文件。

---
+++ LogDemo.Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogDemo.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ LogDemo/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LogDemo
+{
+    public class Class1
+    {
+    }
+}
