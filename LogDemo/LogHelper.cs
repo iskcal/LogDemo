a6db54237e463216ba9c679a0727c113f3fdb7ce
iskcal <iskcal@outlook.com>
2020-08-28 15:51:04 +0800

    v1

---
+++ LogDemo/LogHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogDemo
+{
+    public class LogHelper
+    {
+        public static void LogToConsole(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
